25a1afa46a3b86e810688130c0508076308f1e71
OliverUgo <morzet4ever@gmail.com>
2023-02-17 15:28:05 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always o (Success)
+ */
+int main (void)
+{
+	char Mention [] = ""Programming is like building a multilingual puzzle\n");
+	puts();
+		return = 0;
+}
