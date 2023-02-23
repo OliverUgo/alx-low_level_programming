f75c59a0373fe96fd3a8b0470dcb016f119907ed
OliverUgo <morzet4ever@gmail.com>
2023-02-22 17:43:41 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char low_alph;
-
-	for (low_alph = 'a'; low_alph <= 'z'; low_alph++)
-	{	_putchar(low_alph);
+	int t;
+	
+	for (t = 1; t  <= 10; t++)
+	{
+		for (low_alph = 'a'; low_alph <= 'z'; low_alph++)
+		{
+			_putchar(low_alph);
+		}
+		{
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
-return (0);
 }
