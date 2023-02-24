ed230897c162f5fabf244207c7706f4db6117cda
OliverUgo <morzet4ever@gmail.com>
2023-02-23 20:53:14 -0800

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints most numbers, from 0 to 9
+ * @i: the character to be checked
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 4 && i != 2)
+		{	_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
