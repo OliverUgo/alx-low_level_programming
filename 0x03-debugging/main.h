e70b69ec26c3002071b274b4e42f0c76bdf017b0
OliverUgo <morzet4ever@gmail.com>
2023-02-23 20:11:05 -0800

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void positive_or_negative(int i);
 int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days(int);
 #endif /* MAIN_H */
