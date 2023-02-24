6970e1d0570660ad1e4f9a2b51b9653395926cbf
OliverUgo <morzet4ever@gmail.com>
2023-02-23 20:33:29 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
  * @i: the character to be checked
  * Return: no return
  */
 void print_numbers(void)
 {
 	int i;
-
+	
 	for (i = 0; i <= 9; i++)
-	{
+	{	
 		_putchar(i + '0');
 	}
 	_putchar('\n');
