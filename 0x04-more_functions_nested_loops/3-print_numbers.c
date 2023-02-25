cce1326acdf56183d37a03edd2534a0b345ad9dc
OliverUgo <morzet4ever@gmail.com>
2023-02-24 16:02:05 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9
- * @i: the character to be checked
+ * the character to be checked
  * Return: no return
  */
 
 void print_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
