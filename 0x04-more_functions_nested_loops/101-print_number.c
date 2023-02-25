f623cba3d53721535ac097b67ffccee6aec06919
OliverUgo <morzet4ever@gmail.com>
2023-02-24 18:05:11 -0800

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_diagonal - a function that prints straight line
+ * @n: the number of times printed
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+		}
+}
