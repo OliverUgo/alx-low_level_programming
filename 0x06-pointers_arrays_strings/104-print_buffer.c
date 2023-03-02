7c619529155fc4c1fd3e215db4bd21dc0fd56ab6
OliverUgo <morzet4ever@gmail.com>
2023-03-01 21:02:17 -0800

    104

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat -  a function that concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: concat strings
+ */
+
